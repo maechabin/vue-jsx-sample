a1e1ae2bf838ec6177d1b74a0dfa8a8fb2fe1386
maechabin <mitoppo@gmail.com>
2018-10-07 04:04:43 +0900

    first commit

---
+++ src/components/Child.vue
@@ -0,0 +1,20 @@
+<script lang="tsx">
+import { VNode, CreateElement } from 'vue';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Child extends Vue {
+  @Prop()
+  props!: (bar: string) => VNode;
+
+  render(h: CreateElement): VNode {
+    const { props } = this;
+    return (
+      <div>
+        <h2>Child</h2>
+        {props("foo")}
+      </div>
+    );
+  }
+}
+</script>
---
+++ src/components/GrandChild.vue
@@ -0,0 +1,20 @@
+<script lang="tsx">
+import { VNode, CreateElement } from 'vue';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class GrandChild extends Vue {
+  @Prop()
+  bar!: string;
+
+  render(h: CreateElement): VNode {
+    const { bar } = this;
+    return (
+      <div>
+        <h3>GrandChild</h3>
+        {bar}
+      </div>
+    );
+  }
+}
+</script>
