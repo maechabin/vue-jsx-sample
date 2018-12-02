866b8aa1123b4c2dbf421e570d33d880589a4f35
maechabin <mitoppo@gmail.com>
2018-12-02 22:57:22 +0900

    children試し

--- src/components/helloworld/HelloWorld.tsx
+++ src/components/helloworld/HelloWorld.tsx
@@ -1,9 +1,9 @@
-import { VNode, CreateElement } from 'vue';
+import { VNode, CreateElement, FunctionalComponentOptions, RenderContext } from 'vue';
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class HelloWorld extends Vue {
-  public world: string = 'world';
+  public world: string = 'World';
 
   public render(h: CreateElement): VNode {
     const { world } = this;
@@ -11,6 +11,28 @@ export default class HelloWorld extends Vue {
       fontSize: '64px',
       color: '#3ab981',
     };
-    return <h1 style={style}>{world}</h1>;
+    return (
+      <h1 style={style}>
+        <Child2>Hello</Child2> <Child2>{world}</Child2>
+      </h1>
+    );
+  }
+}
+
+// tslint:disable:max-classes-per-file
+@Component
+class Child extends Vue {
+  public render(h: CreateElement): VNode {
+    return <span>{this.$slots.default[0]}</span>;
+  }
+}
+
+// tslint:disable:max-classes-per-file
+@Component({
+  functional: true,
+} as FunctionalComponentOptions)
+class Child2 extends Vue {
+  public render(h: CreateElement, context: RenderContext): VNode {
+    return <span>{context.children[0]}</span>;
   }
 }
